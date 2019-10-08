43a72736561884fe444712836163b01ef76eea12
chadolskii123 <chadolskii123@gmail.com>
2019-10-08 20:08:05 +0900

    branch added

--- semiFinal/src/semiFinal/Class5G.java
+++ semiFinal/src/semiFinal/Class5G.java
@@ -4,7 +4,13 @@ public class Class5G {
 
 	public static void main(String[] args) {
 		System.out.println("우리는 3조");
-
+		
+		
+		
+		
+		
+		System.out.println( " t저ㅐ웃ㅇ이 ㅔ스트합니다 ");
+		System.out.println(" 이게 웃인게 ");
 	}
 
 }
